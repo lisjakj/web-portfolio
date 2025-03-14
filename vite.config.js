899239be374d002d0bf1d50355d23d7cbe81c03d
lisjakj <jakov.lisjak.jl@gmail.com>
2025-03-14 04:32:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Ovdje eksplicitno definiramo da se "react-helmet-async" ne bundla
+      // i tretira kao "vanjski" modul
+      external: ['react-helmet-async']
+    }
+  }
 })
