7e589d933049dfdaef1e834868c6b8f0e1fb1c90
lisjakj <jakov.lisjak.jl@gmail.com>
2025-03-18 01:27:09 +0100

    NutriPlan
    Dodan novi projekt

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,28 +1,35 @@
-import styles from './ProjectsStyles.module.css'
-import viberr from '../../assets/brziotkup.png'
-import vanzemljac from '../../assets/heartistry.png'
-import ProjectCard from '../../common/ProjectCard'
+import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/brziotkup.png";
+import vanzemljac from "../../assets/heartistry.png";
+import nutriplan from "../../assets/nutriplan.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-        <h1 className="sectionTitle">Projekti</h1>
-        <div className={styles.ProjectsContainer}>
-            <ProjectCard 
-            src={viberr} 
-            link="https://brziotkupvozila.com"
-            h3="Otkup vozila"
-            p="Stranica za profesionalni otkup vozila"
-            />
-            <ProjectCard 
-            src={vanzemljac} 
-            link="https://limun.netlify.app"
-            h3="Web shop"
-            p="Dropshipping webshop sa nakitom"
-            />
-        </div>
+      <h1 className="sectionTitle">Projekti</h1>
+      <div className={styles.ProjectsContainer}>
+        <ProjectCard
+          src={nutriplan}
+          link="https://modest-guests-593738.framer.app"
+          h3="NutriPlan"
+          p="Usluga zdrave prehrane"
+        />
+        <ProjectCard
+          src={viberr}
+          link="https://brziotkupvozila.com"
+          h3="Otkup vozila"
+          p="Stranica za profesionalni otkup vozila"
+        />
+        <ProjectCard
+          src={vanzemljac}
+          link="https://limun.netlify.app"
+          h3="Web shop"
+          p="Dropshipping webshop sa nakitom"
+        />
+      </div>
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
